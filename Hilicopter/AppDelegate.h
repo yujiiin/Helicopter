211255bb9f0aa0d8dcb05325ee42a6cbff75750b
Yuji Shimizu <yuji.shimizu@dena.com>
2014-01-12 11:37:21 -0800

    Initial Commit

---
+++ Hilicopter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hilicopter
+//
+//  Created by Yuji Shimizu on 1/12/14.
+//  Copyright (c) 2014 Yuji Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
